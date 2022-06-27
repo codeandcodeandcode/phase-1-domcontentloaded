615084dd547a09cd97a72914a6a15a3c6c9e4788
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 03:39:47 +0300

    Got the paragraph and changed it's text

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 //Added domconent eventlistener
 
 document.addEventListener("DOMContentLoaded", function() {
-
+    //Got the paragraph and changed it's text
+    document.querySelector("p#text").textContent = "This is really cool!";
 });
